545bfda1ae1e1d3064e785b53b49546686df0c43
PedroHenrique-git <pedromf34@outlook.com>
2023-03-05 21:01:28 -0300

    feat: improvements db

--- src/shared/helpers/handlePrismaError.ts
+++ src/shared/helpers/handlePrismaError.ts
@@ -1,15 +1,19 @@
 import { Prisma } from '@prisma/client';
+import { DEFAULT_ERROR_MESSAGE } from '../constants';
 
 type Error = { error: boolean; message: string };
 
 export function handlePrismaError(err: any): Error {
   if (err instanceof Prisma.PrismaClientKnownRequestError) {
-    return { message: String(err.meta.cause), error: true };
+    return {
+      message: String(err.meta.cause ?? DEFAULT_ERROR_MESSAGE),
+      error: true,
+    };
   }
 
   if (err instanceof Prisma.PrismaClientUnknownRequestError) {
-    return { message: String(err.name), error: true };
+    return { message: String(err.name ?? DEFAULT_ERROR_MESSAGE), error: true };
   }
 
-  return { message: String(err.message), error: true };
+  return { message: String(err.message ?? DEFAULT_ERROR_MESSAGE), error: true };
 }
