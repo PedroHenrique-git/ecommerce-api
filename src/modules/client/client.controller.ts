468a085d5b2fb1e281c48196064475e8d3d8f76d
PedroHenrique-git <pedromf34@outlook.com>
2023-03-21 19:14:41 -0300

    fix: sort default value

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -13,7 +13,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-import { DEFAULT_PAGE, DEFAULT_TAKE } from 'src/shared/constants';
+import { DEFAULT_PAGE, DEFAULT_SORT, DEFAULT_TAKE } from 'src/shared/constants';
 import { ValidationSchemaPipe } from 'src/shared/pipes/validation-schema.pipe';
 import { Role } from 'src/shared/protocols/role.enum';
 import { Public } from '../auth/decorators/public-route.decorator';
@@ -112,7 +112,7 @@ export class ClientController {
     page: number,
     @Query('take', new DefaultValuePipe(DEFAULT_TAKE), ParseIntPipe)
     take: number,
-    @Query('sort', new DefaultValuePipe(DEFAULT_TAKE))
+    @Query('sort', new DefaultValuePipe(DEFAULT_SORT))
     sort: Prisma.SortOrder,
   ) {
     try {
